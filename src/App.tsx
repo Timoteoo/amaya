77ee97430a9dda3d386263dd0a96dc03c2c3b12e
LureDev <randomthecoder1557@gmail.com>
2021-10-10 16:13:28 -0300

    Added routes for Home and Profile page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
 import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
 
 import "./styles/global.scss";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/profile" component={Profile} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
