d90c9b0d71d7f61c3edd832490c50e162f7e23ad
LureDev <randomthecoder1557@gmail.com>
2021-10-10 16:04:19 -0300

    Made Home page and its styles.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,36 @@
 import { Navbar } from "../components/Navbar";
 
+import "../styles/home.scss";
+
 export const Home = () => {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <main id="home">
+        <div className="title">
+          <h1>Create Profile</h1>
+          <div className="big-bar"></div>
+        </div>
+        <p>Set your profile name and image!</p>
+
+        <form autoComplete="off">
+          <div className="input-wraper">
+            <div className="input-area">
+              <input type="text" id="username" required />
+              <label htmlFor="username">Username</label>
+            </div>
+          </div>
+
+          <div className="input-wraper">
+            <div className="input-area">
+              <input type="text" id="image" required />
+              <label htmlFor="image">Image (URL)</label>
+            </div>
+          </div>
+
+          <button>Create</button>
+        </form>
+      </main>
+    </div>
+  );
 };
