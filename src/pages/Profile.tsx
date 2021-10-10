c9796dbbaa9c2348d0294ddbe85ccbb3f91f6298
LureDev <randomthecoder1557@gmail.com>
2021-10-10 17:33:43 -0300

    Removed unnecessary word from profile image alt.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -9,7 +9,7 @@ export const Profile = () => {
       <main id="profile">
         <img
           src="https://cdn.discordapp.com/avatars/882750293937389629/b89de856d0c70f0310b1edf29d03aaab.webp?size=80"
-          alt="Profile image"
+          alt="Profile"
         />
         <h2>Lure</h2>
       </main>
