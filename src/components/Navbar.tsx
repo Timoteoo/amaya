ed35df440265ddf874339fb14e294b23afb11f69
LureDev <randomthecoder1557@gmail.com>
2021-10-10 15:26:11 -0300

    Changed Navbar svg color.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     <div>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
-          fill="#7a5bea"
+          fill="#e04a7e"
           fill-opacity="1"
           d="M0,288L120,240C240,192,480,96,720,85.3C960,75,1200,149,1320,186.7L1440,224L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
         ></path>
