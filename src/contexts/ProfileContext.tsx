c4a3b6a8b6cd6acafee152d9e6684054260b6291
LureDev <randomthecoder1557@gmail.com>
2021-10-10 17:19:15 -0300

    Made profile context for sharing profile info between components.

---
+++ src/contexts/ProfileContext.tsx
@@ -0,0 +1,34 @@
+import { useState, createContext, ReactNode } from "react";
+
+type ProfileType = {
+  username: string;
+  image: string;
+};
+
+type ProfileContextType = {
+  profileData: ProfileType | undefined;
+  changeProfileData: ({}: ProfileType) => void;
+};
+
+type ProfileContextProviderProps = {
+  children: ReactNode;
+};
+
+export const ProfileContext = createContext({} as ProfileContextType);
+
+export const ProfileContextProvider = (props: ProfileContextProviderProps) => {
+  const [profileData, setProfileData] = useState<ProfileType>();
+
+  const changeProfileData = ({ username, image }: ProfileType) => {
+    setProfileData({
+      username,
+      image,
+    });
+  };
+
+  return (
+    <ProfileContext.Provider value={{ profileData, changeProfileData }}>
+      {props.children}
+    </ProfileContext.Provider>
+  );
+};
