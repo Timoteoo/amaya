e9921b31eec0a6683bfbefa03e44fe3a382a3077
LureDev <randomthecoder1557@gmail.com>
2021-10-10 17:32:46 -0300

    Implemented profile context in Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,29 @@
+import { FormEvent, useState, useContext } from "react";
+
+import { ProfileContext } from "../contexts/ProfileContext";
+
 import { Navbar } from "../components/Navbar";
 
 import "../styles/home.scss";
 
 export const Home = () => {
+  const [username, setUsername] = useState("");
+  const [image, setImage] = useState("");
+
+  const { changeProfileData } = useContext(ProfileContext);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!username || !image || username.trim() === "" || image.trim() === "")
+      return;
+
+    changeProfileData({
+      username,
+      image,
+    });
+  };
+
   return (
     <div>
       <Navbar />
@@ -13,17 +34,29 @@ export const Home = () => {
         </div>
         <p>Set your profile name and image!</p>
 
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={e => handleSubmit(e)}>
           <div className="input-wraper">
             <div className="input-area">
-              <input type="text" id="username" required />
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                required
+              />
               <label htmlFor="username">Username</label>
             </div>
           </div>
 
           <div className="input-wraper">
             <div className="input-area">
-              <input type="text" id="image" required />
+              <input
+                type="text"
+                id="image"
+                value={image}
+                onChange={e => setImage(e.target.value)}
+                required
+              />
               <label htmlFor="image">Image (URL)</label>
             </div>
           </div>
